f27fae99fbec95cdd252fe3fe3fe407de94972ed
ramosmicael <ramosmicael@users.noreply.github.com>
2019-08-02 00:55:11 +0100

    Update VidyoIOPlugin.js

--- www/VidyoIOPlugin.js
+++ www/VidyoIOPlugin.js
@@ -1,11 +1,6 @@
+
 var exec = require('cordova/exec');
-
 function plugin() {
-
 }
-
-plugin.prototype.launchVidyoIO = function(args) {
-    exec(function(res){}, function(err){}, "VidyoIOPlugin", "launchVidyoIO", args);
-}
-
+plugin.prototype.launchVidyoIO = function(args) { exec(function(res){}, function(err){}, "VidyoIOPlugin", "launchVidyoIO", args); }
 module.exports = new plugin();
